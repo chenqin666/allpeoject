34ec7bfc177399ba4648980d1511f6ab01f2915c
陈芹1234 <13031117181@163.com>
2021-03-16 14:37:26 +0800

    cq

--- 人脸识别小程序/face-recognition/utils/api.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  wxlogin:'wechat.execute.auth',
-  logins:'v1.account/authorize',
-  uploadeimg:'v1.upload/image',
-  search:'v1.account/search'
-
-} 
--- 支付宝小程序/支付宝人脸识别/face/utils/api.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  wxlogin:'zhifubao.execute.auth',
-  verifycode:'verify.code.get',
-  verification:'verify.code.validate',
-  bind:'member.miniprogram.bind',
-  login:'member.mobile.login',
-  query:'general.info.query',
-  bindcards:"member.bind.cards",
-} 
